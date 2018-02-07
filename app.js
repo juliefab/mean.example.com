6b8ea23f452e2bd9617ef9c3eb8acac66b145c0c
juliefab <jfinch1@artic.edu>
2018-02-07 10:54:57 -0600

    Added configuration variables

--- app.js
+++ app.js
@@ -44,12 +44,12 @@ var MongoStore = require('connect-mongo')(session);
 
 app.use(require('express-session')({
   store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  secret:'.?Qn28B>s|A{Vz~(w;hX;8v3Us$\H;[)|8(KH(HUNaW<*;:AI@h{`&pA~o|&uAj',
+  secret: config.secret,
   resave: false,
   saveUninitialized: false,
   cookie: {
     path: '/',
-    domain: 'localhost',
+    domain: config.cookie.domain,
     //domain: 'localhost',
     //httpOnly: true,
     //secure: true,
@@ -82,9 +82,9 @@ app.use(function(req, res, next) {
 passport.use(User.createStrategy());
 
 passport.use(new GitHubStrategy({
-    clientID:'7e787b757d47bded93e6',
-    clientSecret:'65d86f8118ef2f5cc3ced6213fb8bddd0337e3f7',
-    callbackURL: 'https://j-finch.com/auth/github/callback'
+    clientID:config.gitHub.clientID,
+    clientSecret:config.gitHub.clientSecret,
+    callbackURL:config.gitHub.callbackURL
   },function(accessToken, refreshToken, profile, cb){
 
     //The ID MUST be cast to an INT
@@ -184,10 +184,10 @@ app.use(function(req,res,next){
   if(req.isAuthenticated()){
     return next();
   }
-    return next();
+    // return next();
   // res.status(401);
   // return res.send('unauthorized');
-  //return res.redirect('/users/login');
+  return res.redirect('/users/login');
 });
 
 app.use('/', index);
