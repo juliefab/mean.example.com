fb15b9f9b19e03728467edb8b061ed354347b2b8
juliefab <jfinch1@artic.edu>
2018-02-15 13:07:12 -0600

    added public view for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var bodyParser = require('body-parser');
+var Post = require('../models/post');
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
-  res.render('posts/index', {title:'CMS'});
+
+router.get('/', function(req, res){
+
+  Post.find({},function(err, posts){
+    if(err){
+    return res.render('posts/index',{'success':false, 'error': err});
+    }
+    return res.render('posts/index',{'success':true, 'posts': posts});
+  });
+
+});
+
+router.get('/view/:slug', function(req,res){
+  var slug = req.params.slug;
+
+  Post.findOne({'slug':slug}, function(err, post){
+
+    if(err){
+      return res.render('posts/view',{'success':false, 'error': err});
+    }
+
+    return res.render('posts/view',{'success':true, 'post': post});
+
+  });
+
 });
 
 
